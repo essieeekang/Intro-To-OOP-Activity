f88fb77cbba3c35911e6dfc46c535f7d14eda853
Esther Kang <estherkang.j@gmail.com>
2025-03-27 11:02:05 -0700

    implements class Student with constructor and methods

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,17 @@
 # add your Student class here!
+
+class Student:
+
+    def __init__(self, name, year, classes):
+        self.name = name
+        self.year = year
+        self.classes = classes
+
+    def add_class(self, new_class):
+        self.classes.append(new_class)
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        print(f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes")
