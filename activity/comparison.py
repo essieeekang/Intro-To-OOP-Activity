d04b0ee8b5f66c23c6f159d2d8dbcc67ff25764b
Stella Sun <stellasun0332@gmail.com>
2025-03-27 13:45:55 -0500

    Implements get_student_with_more_classes.

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,9 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(student_a, student_b):
+    if student_a.get_num_classes() > student_b.get_num_classes():
+        return student_a.name
+    elif student_a.get_num_classes() == student_b.get_num_classes():
+        print(f'{student_a.name} and {student_b.name} have the same number of classes.') 
+    else:
+        return student_b.name
+    
